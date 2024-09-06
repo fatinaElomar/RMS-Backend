433deb0043def1c978023bad09d7f3b272f33b42
fatinaElomar <fatina-el-omar@hotmail.co.uk>
2024-09-06 09:30:45 +0300

    DB connected

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config(); // Import dotenv for environment variables
+
 const app = express();
 const port = 3000;
 
 // Middleware
 app.use(express.json());
+
+// MongoDB cluster connection with Mongoose
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connect(MONGO_URL)
+  .then(() => console.log('MongoDB cluster connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Define a route
 app.get('/', (req, res) => {
